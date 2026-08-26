1f52ee37471fe4865bdfb1d84869490c57ef5c69
pksmoothie <patrickkeenan21@gmail.com>
2026-08-25 23:35:48 -0400

    Bump PatBot to v0.4.7 and install live roster identity context

--- patbot/__init__.py
+++ patbot/__init__.py
@@ -1,7 +1,9 @@
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 
 from .opponent_history import install_owner_history_patch
 from .roster_strategy import install_roster_strategy_patch
+from .live_roster_context import install_live_roster_context_patch
 
 install_owner_history_patch()
 install_roster_strategy_patch()
+install_live_roster_context_patch()
