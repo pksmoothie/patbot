8eb2a81b3241ece3f3b91e92eefdf5d9e6fbfe59
pksmoothie <patrickkeenan21@gmail.com>
2026-08-25 22:54:13 -0400

    Enable value-aware roster construction

--- patbot/__init__.py
+++ patbot/__init__.py
@@ -1,5 +1,7 @@
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 
 from .opponent_history import install_owner_history_patch
+from .roster_strategy import install_roster_strategy_patch
 
 install_owner_history_patch()
+install_roster_strategy_patch()
