8bb414066439e7e9c0b14f253382c2a14f77b8b1
pksmoothie <patrickkeenan21@gmail.com>
2026-08-27 23:18:50 -0400

    v0.5.7 install late-round expert upside production patch

--- patbot/__init__.py
+++ patbot/__init__.py
@@ -1,9 +1,11 @@
-__version__ = "0.5.6"
+__version__ = "0.5.7"
 
 from .opponent_history import install_owner_history_patch
 from .roster_strategy import install_roster_strategy_patch
 from .live_roster_context import install_live_roster_context_patch
+from .expert_upside_production import install_expert_upside_production_patch
 
 install_owner_history_patch()
 install_roster_strategy_patch()
 install_live_roster_context_patch()
+install_expert_upside_production_patch()
