1187e03ccdf98b30cbb7824371e67de9e824d9b8
pksmoothie <patrickkeenan21@gmail.com>
2026-08-31 23:58:43 -0400

    Install v0.6.13 opponent availability patch

--- patbot/__init__.py
+++ patbot/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.6.12"
+__version__ = "0.6.13"
 
 from .opponent_history import install_owner_history_patch
 from .roster_strategy import install_roster_strategy_patch
@@ -7,6 +7,7 @@ from .expert_upside_production import install_expert_upside_production_patch
 from .yahoo_room_behavior import install_yahoo_room_behavior_patch
 from .team_concentration import install_team_concentration_patch
 from .decision_strategy import install_decision_strategy_patch
+from .opponent_availability import install_opponent_availability_patch
 
 install_owner_history_patch()
 install_roster_strategy_patch()
@@ -15,3 +16,4 @@ install_expert_upside_production_patch()
 install_yahoo_room_behavior_patch()
 install_team_concentration_patch()
 install_decision_strategy_patch()
+install_opponent_availability_patch()
